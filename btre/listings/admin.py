9d451de9e527478e0b7ac57bc0fbeaa208a724f5
Bankole Esan <techybanky@gmail.com>
2019-11-25 10:31:28 +0100

    edited made home page

--- btre/listings/admin.py
+++ btre/listings/admin.py
@@ -3,4 +3,13 @@ from .models import Listing
 
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin): 
+  list_display = ('id', 'title', 'is_published','price','list_date','realtor')
+  list_display_links = ('id','title')
+  list_editable = ('is_published',)
+  list_filter = ('realtor',)
+  search_fields = ('title', 'description','address','city','state','zipcode','price')
+  list_per_page = 25 # pagination
+
+
+admin.site.register(Listing, ListingAdmin)
--- btre/realtors/admin.py
+++ btre/realtors/admin.py
@@ -3,4 +3,13 @@ from .models import Realtor
 
 # Register your models here.
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin): 
+  list_display = ('id','name','is_mvp','phone','email')
+  list_display_links = ('id','name')
+  list_editable = ('is_mvp',)
+  list_filter = ('name',)
+  search_fields = ('name','description','phone','email','hire_date')
+  list_per_page = 10
+
+
+admin.site.register(Realtor, RealtorAdmin)
