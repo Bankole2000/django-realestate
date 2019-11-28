0df4deedcdad6a71ed1cd188c75d5b6aeb8248fd
Bankole Esan <techybanky@gmail.com>
2019-11-28 06:26:51 +0100

    added search page

--- btre/pages/views.py
+++ btre/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 
@@ -7,13 +8,19 @@ from realtors.models import Realtor
 def index(request):
   listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
   context = {
-    'listings': listings
+    'listings': listings,
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices,
   } 
   return render(request, 'pages/index.html', context)
 
 def about(request): 
-  realtor = Realtor.objects.order_by('-hire_date').filter(is_mvp=True)[:1]
+  realtors = Realtor.objects.order_by('-hire_date')
+  # Get MVP 
+  mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
   context = {
-    'realtor' : realtor
+    'realtors' : realtors,
+    'mvp_realtors' : mvp_realtors
   }
   return render(request, 'pages/about.html', context)
