fb7710740ba79a3ee42572b259c2072809bbeec3
Bankole Esan <techybanky@gmail.com>
2019-11-29 18:42:37 +0100

    adding search functionality

--- btre/listings/views.py
+++ btre/listings/views.py
@@ -22,11 +22,44 @@ def listing(request, listing_id):
   return render(request, 'listings/listing.html', context)
 
 def search(request): 
-  listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+  queryset_list = Listing.objects.order_by('-list_date')
+
+  # Keyword search 
+  if 'keywords' in request.GET: 
+    keywords = request.GET['keywords']
+    if keywords: 
+      queryset_list = queryset_list.filter(description__icontains=keywords)
+
+  # city 
+  if 'city' in request.GET: 
+    city = request.GET['city']
+    if city: 
+      queryset_list = queryset_list.filter(city__iexact=city)
+
+  # state
+  if 'state' in request.GET: 
+    state = request.GET['state']
+    if state: 
+      queryset_list = queryset_list.filter(state__iexact=state)
+
+  # bedrooms
+  if 'bedrooms' in request.GET: 
+    bedrooms = request.GET['bedrooms']
+    if bedrooms: 
+      queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+  # price
+  if 'price' in request.GET: 
+    price = request.GET['price']
+    if price: 
+      queryset_list = queryset_list.filter(price__lte=price)
+
+
   context = {
-    'listings': listings,
+    'listings': queryset_list,
     'state_choices': state_choices,
     'bedroom_choices': bedroom_choices,
     'price_choices': price_choices,
+    'values': request.GET
   } 
   return render(request, 'listings/search.html', context)
